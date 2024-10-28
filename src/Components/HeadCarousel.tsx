9c75f7d423876f59fd5649620bb7c11ace6b8283
levan.khvedelidze <bako.rap@gmail.com>
2024-10-28 21:42:14 +0400

    add scroll function

--- src/Components/HeadCarousel.tsx
+++ src/Components/HeadCarousel.tsx
@@ -6,7 +6,7 @@ const HeadCarousel = ()=> {
 
     return(
     
-        <section className="w-[100%] flex flex-col items-center mb-[160px]">
+        <section className="w-[100%] flex flex-col items-center mt-[160px]">
             <h2 className="text-[#2D67D8] font-black text-[36px]">
                 ექსპერტები, რომლებიც წარმართავენ 
             </h2>
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -2,6 +2,43 @@ import LineDev from "../assets/Photos/Header/LINDEV.png"
 
 
 const Navbar = ()=>{
+
+
+    function scrollToAbout() {
+        window.scrollTo({
+          top: 748,
+          behavior: "smooth"
+        });
+      }
+
+      function scrollToCourses() {
+        window.scrollTo({
+          top: 2100,
+          behavior: "smooth"
+        });
+      }
+
+      function scrollToblog() {
+        window.scrollTo({
+          top: 2540,
+          behavior: "smooth"
+        });
+      }
+      function scrollToContact() {
+        window.scrollTo({
+          top: 2980,
+          behavior: "smooth"
+        });
+      }
+
+      function scrollToNews() {
+        window.scrollTo({
+          top: 1400,
+          behavior: "smooth"
+        });
+      }
+
+
     return(
 
         <section className="w-9/12 h-9 flex justify-between m-auto my-7">
@@ -10,11 +47,11 @@ const Navbar = ()=>{
 
             <nav className="w-2/4 ">
                 <ul className="flex justify-between">
-                    <li className="cursor-pointer">კურსები</li>
-                    <li className="cursor-pointer">ჩვენს შესახებ</li>
-                    <li className="cursor-pointer">ბლოგი</li>
-                    <li className="cursor-pointer">სიახლეები</li>
-                    <li className="cursor-pointer">კონტაქტი</li>
+                    <li onClick={scrollToCourses} className="cursor-pointer">კურსები</li>
+                    <li onClick={scrollToAbout} className="cursor-pointer">ჩვენს შესახებ</li>
+                    <li onClick={scrollToblog} className="cursor-pointer">ბლოგი</li>
+                    <li onClick={scrollToNews} className="cursor-pointer">სიახლეები</li>
+                    <li onClick={scrollToContact} className="cursor-pointer">კონტაქტი</li>
                 </ul>
             </nav>
 
